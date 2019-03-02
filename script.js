9193e1d2c9b4af6b2bf49b5a6de16ee079b56455
bilal <mbilalghz@gmail.com>
2019-03-02 19:29:20 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+var tUbahWarna = document.getElementById('lingkaran1')
+tUbahWarna.onclick = function() {
+    document.body.style.backgroundColor = 'lightcoral'
+}
+var tUbahWarna = document.getElementById('lingkaran2')
+tUbahWarna.onclick = function() {
+    document.body.style.backgroundColor = 'lightslategrey'
+}
+var tUbahWarna = document.getElementById('lingkaran3')
+tUbahWarna.onclick = function() {
+    document.body.style.backgroundColor = 'lightseagreen'
+}
+var tUbahWarna = document.getElementById('lingkaran4')
+tUbahWarna.onclick = function() {
+    document.body.style.backgroundColor = 'lightgreen'
+}
+var tUbahWarna = document.getElementById('lingkaran5')
+tUbahWarna.onclick = function() {
+    document.body.style.backgroundColor = 'lightblue'
+}
